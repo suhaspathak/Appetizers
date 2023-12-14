532291cb91eec9d239ec74d942a40874a242d947
suhas pathak <suhas.jio46@gmail.com>
2023-12-14 06:22:15 +0530

    Added View Model and Alet to show the error messages.

--- LoadingView.swift
+++ LoadingView.swift
@@ -5,4 +5,20 @@
 //  Created by Suhas Pathak on 14/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let acttivityIndicator = UIActivityIndicatorView(style: .large)
+        acttivityIndicator.color = UIColor.brandPrimary1
+        acttivityIndicator.startAnimating()
+        return acttivityIndicator
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+    }
+    
+   
+    
+    
+}
